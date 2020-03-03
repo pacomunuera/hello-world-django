1a3ee4e58aba287794cb78ea3ccc06e28af4177e
pacomunuera <46718931+pacomunuera@users.noreply.github.com>
2020-03-03 13:03:04 +0100

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', views.homePageView, name='home'),
+]
